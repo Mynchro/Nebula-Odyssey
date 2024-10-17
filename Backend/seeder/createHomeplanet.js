dd737901341f59a462b2c0da6c7fe3671687f68c
Mynchro <webermarius1995@gmail.com>
2024-10-17 14:35:55 +0200

    DB Struktur und Funktionalität

---
+++ Backend/seeder/createHomeplanet.js
@@ -0,0 +1,35 @@
+import Planet from "../models/Planet.js";
+import { Resource } from "../models/Resources.js";
+import Building from "../models/Buildings.js";
+
+export const createHomeplanet = async (userId) => {
+  try {
+    const defaultBuildings = await Building.find();
+    const defaultResources = await Resource.findOne();
+
+    if (!defaultResources || !defaultBuildings) {
+      throw new Error(
+        "Keine Ressourcen oder Gebäude in der Datenbank gefunden!"
+      );
+    }
+
+    const newPlanet = new Planet({
+      owner: userId,
+      name: "Nebula",
+      buildings: defaultBuildings.map((building) => ({
+        originalBuildingId: building._id,
+        buildingType: building.buildingType,
+        level: building.level,
+        productionRate: building.productionRate,
+      })),
+      resources: defaultResources._id,
+    });
+
+    await newPlanet.save();
+
+    console.log("Heimatplanet erfolgreich erstellt!");
+    return newPlanet;
+  } catch (error) {
+    console.error("Fehler beim Erstellen des Planeten:", error);
+  }
+};
