47668f918c814d692ce31f358a9b1fbe36c7b6a2
Mynchro <webermarius1995@gmail.com>
2024-11-07 09:09:58 +0100

    provider überarbeitet, app.jsx struktur fix

--- Frontend/src/context/PlayerContext.jsx
+++ Frontend/src/context/PlayerContext.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const PlayerContext = createContext();
 export const defaultUser_DEV = {
@@ -12,39 +13,40 @@ const PlayerProvider = ({ children }) => {
   const [currentPlayer, setCurrentPlayer] = useState({
     user: defaultUser_DEV,
   });
+  const navigate = useNavigate();
 
-  const fetchPlayerData = async (userId) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/user/${userId}`, {
-        credentials: "include",
-      });
-      if (response.ok) {
-        const data = await response.json();
-        console.log("data:", data);
-        setPlayerData(data);
-        setCurrentPlayer({
-          userName: data.userName,
-          settings: data.settings,
-          planets: data.planets,
-          _id: data._id,
-        });
-      }
-      // const data = await response.json();
+  // const fetchPlayerData = async (userId) => {
+  //   try {
+  //     const response = await fetch(`http://localhost:3000/api/user/${userId}`, {
+  //       credentials: "include",
+  //     });
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       console.log("data:", data);
+  //       setPlayerData(data);
+  //       setCurrentPlayer({
+  //         userName: data.userName,
+  //         settings: data.settings,
+  //         planets: data.planets,
+  //         _id: data._id,
+  //       });
+  //     }
+  //     // const data = await response.json();
 
-      // setPlayerData(data);
+  //     // setPlayerData(data);
 
-      // setCurrentPlayer((prev) => ({
-      //   ...prev,
-      //   user: {
-      //     ...prev.user,
-      //     settings: data.settings, // Nehme die Einstellungen aus der Datenbank
-      //     planets: data.planets, // Füge die Planeten des Benutzers hinzu
-      //   },
-      // }));
-    } catch (error) {
-      console.error("Fehler beim Abrufen der Spieldaten:", error);
-    }
-  };
+  //     // setCurrentPlayer((prev) => ({
+  //     //   ...prev,
+  //     //   user: {
+  //     //     ...prev.user,
+  //     //     settings: data.settings, // Nehme die Einstellungen aus der Datenbank
+  //     //     planets: data.planets, // Füge die Planeten des Benutzers hinzu
+  //     //   },
+  //     // }));
+  //   } catch (error) {
+  //     console.error("Fehler beim Abrufen der Spieldaten:", error);
+  //   }
+  // };
 
   const handleLogin = async (credentials) => {
     try {
@@ -60,15 +62,16 @@ const PlayerProvider = ({ children }) => {
       if (response.ok) {
         const result = await response.json();
         console.log("result:", result); // Überprüfe das Ergebnis hier
+        setCurrentPlayer(result.user);
 
-        if (result.user && result.user._id) {
-          // await fetchPlayerData(result.user._id);
-          setCurrentPlayer(result.user); // Hier die userId angeben
-          console.log("currentPlayer:", currentPlayer);
-          console.log("Aktueller Spieler nach dem Login:", result.user); // Nutzerinformationen überprüfen
-        } else {
-          console.error("Benutzerinformationen fehlen im Ergebnis.");
-        }
+        // if (result.user && result.user._id) {
+        //   // await fetchPlayerData(result.user._id);
+        //   setCurrentPlayer(result.user); // Hier die userId angeben
+        console.log("currentPlayer:", currentPlayer);
+        console.log("Aktueller Spieler nach dem Login:", result.user); // Nutzerinformationen überprüfen
+        // } else {
+        //   console.error("Benutzerinformationen fehlen im Ergebnis.");
+        // }
       } else {
         console.error("Login fehlgeschlagen:", response.statusText);
       }
@@ -87,12 +90,9 @@ const PlayerProvider = ({ children }) => {
       );
       if (response.ok) {
         const data = await response.json();
-        if (data && data._id) {
-          await fetchPlayerData(data._id);
-        } else {
-          console.log("Kein eingeloggter Benutzer gefunden.");
-        }
+        setCurrentPlayer(data);
       } else {
+        navigate("/");
         console.log("Keine Benutzerdaten gefunden:", response.statusText);
       }
     } catch (error) {
