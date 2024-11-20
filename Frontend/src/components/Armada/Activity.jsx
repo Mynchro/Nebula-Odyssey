799ce5807731c9ab7f546ac9fd1730478f47dbb1
Mynchro <webermarius1995@gmail.com>
2024-11-20 17:25:41 +0100

    eslint disable activity.jsx

--- Frontend/src/components/Armada/Activity.jsx
+++ Frontend/src/components/Armada/Activity.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { useState, useEffect, useContext } from "react";
 import { PlayerContext } from "../../context/PlayerContext";
