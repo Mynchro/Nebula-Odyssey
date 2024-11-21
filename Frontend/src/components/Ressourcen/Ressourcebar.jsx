7ed38ca2e27077a8c2fc0d7652ffba0354c28610
Mynchro <webermarius1995@gmail.com>
2024-11-21 11:57:36 +0100

    eslint disable

--- Frontend/src/components/Ressourcen/Ressourcebar.jsx
+++ Frontend/src/components/Ressourcen/Ressourcebar.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
 import "./Ressourcebar.css";
 
 const Ressourcebar = ({ resources }) => {
